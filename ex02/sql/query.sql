5d1c78ded42ed6b91dc0e2f6e66e1e1b18505054
Class01 <Class01@DESKTOP-DC2BM99>
2023-08-07 09:53:17 +0900

    z

--- ex02/sql/query.sql
+++ ex02/sql/query.sql
@@ -10,3 +10,11 @@ CREATE TABLE tbl_board(
  regdate DATE DEFAULT sysdate,
  updatedate DATE DEFAULT sysdate
 );
+
+ALTER TABLE TBL_BOARD ADD CONSTRAINT pk_board
+PRIMARY KEY (bno);
+
+SELECT * FROM TBL_BOARD;
+
+INSERT INTO TBL_BOARD (bno, title, content, writer)
+VALUES (seq_board.nextval, '테스트 제목', '테스트 내용', 'user00');
